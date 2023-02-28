09a741f2fbad4a3b85d82bbfce461e4c019bd4b5
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-02-28 14:55:13 -0300

    Extra checks to prevent false positives

--- main.ts
+++ main.ts
@@ -1,20 +1,24 @@
 import { MarkdownPostProcessor, Plugin } from "obsidian";
 
 const markdownPostProcessor: MarkdownPostProcessor = (element) => {
+	// Find all links
+	const prefix = "app://obsidian.md/";
 	const linkElements = element.querySelectorAll("a.internal-link") as NodeListOf<HTMLAnchorElement>;
-	for (const linkElement of linkElements) {
-		const lastIndex = linkElement.innerText.lastIndexOf("/");
-		if (lastIndex < 0) {
-			continue;
+	for (const el of linkElements) {
+		// If the link text is the same as the href (i.e. user hasn't made a explicit title)
+		const href = decodeURI(el.href);
+		if (href.startsWith(prefix) && href.replace(prefix, "") == el.innerText) {
+			// The text should start right after the last '/' character
+			const lastIndex = el.innerText.lastIndexOf("/");
+			if (lastIndex >= 0) {
+				el.innerText = el.innerText.substring(lastIndex + 1);
+			}
 		}
-		// The text should start right after the last '/'
-		linkElement.innerText = linkElement.innerText.substring(lastIndex + 1);
 	}
 };
 
 export default class extends Plugin {
 	override async onload(): Promise<void> {
-		// this.registerEditorExtension(editorExtension);
 		this.registerMarkdownPostProcessor(markdownPostProcessor);
 	}
 }
