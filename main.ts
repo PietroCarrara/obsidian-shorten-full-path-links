30b7cb20bc520497ae1d7578a8d9f92f88a32ad3
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-02-28 20:10:23 -0300

    Adapt code to work on android

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import { MarkdownPostProcessor, Plugin } from "obsidian";
 
 const markdownPostProcessor: MarkdownPostProcessor = (element) => {
 	// Find all links
-	const prefix = "app://obsidian.md/";
+	const prefixes = ["app://obsidian.md/", "http://localhost/"];
 	const linkElements = element.querySelectorAll("a.internal-link") as NodeListOf<HTMLAnchorElement>;
 	for (const el of linkElements) {
 		// If the link text is the same as the href (i.e. user hasn't made a explicit title)
 		const href = decodeURI(el.href);
-		if (href.startsWith(prefix) && href.replace(prefix, "") == el.innerText) {
+		if (prefixes.map(p => href.startsWith(p) ? href.replace(p, "") == el.innerText : false).reduce((a, b) => a || b)) {
 			// The text should start right after the last '/' character
 			const lastIndex = el.innerText.lastIndexOf("/");
 			if (lastIndex >= 0) {
